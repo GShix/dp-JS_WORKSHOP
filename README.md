ddbedd551d34e375a61ab5104c965c3ac387e490
Dambar Sing <programmer.gshi18@gmail.com>
2024-02-24 12:02:42 +0545

    Variables , Array & Objects

---
+++ README.md
@@ -0,0 +1,12 @@
+First JS Workshop:
+
+Day 0: History about JS & QA
+Day 1: Variable & Data Types: variable maa element unsequentially basxa
+        i) var - reassignment garna milxa, global & functional scope
+        ii) let - reassignment garna milxa, block level scope
+        iii) const - mildaina, block level scope
+
+    Array: Sequential is element basxa, separated by byte taken each element 
+           Array le data ko information khulaudaina: Solution-> Object
+
+    Object: Data Stored in key:value pair
--- day1.js
+++ day1.js
@@ -1,1 +1,29 @@
-console.log("This is my first JS Workshop")
+// console.log("This is my first JS Workshop")
+
+// let cup = "Coffee"
+// cup ="Water"
+// console.log(cup)
+
+//var vs let vs const
+// if(true){
+//     var name = "Dambar" //not with let & const 
+//     console.log(name + 10 + " Gharti")
+// }
+// let name; // declare -> undefined
+// console.log(name,"This is second")
+
+//boolean
+// const isNepali = true // or false
+
+//array
+// let friends = ['DigitalPa','Bob','John'] // different data type in dina milxa (JS is dynamic)
+// let offer = ['Digital Pathshala',999] 
+
+// console.log(offer) // Problem: Array don't share information about offer
+
+//Solution: Object
+let offers ={
+    institueName: "Digital Pathshala",
+    reactJS: "At Rs."+  999
+}
+console.log(offers.reactJS)
