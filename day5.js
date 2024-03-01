b1c368685b174cdb1ca0858235c5190f39af7017
Dambar Sing <programmer.gshi18@gmail.com>
2024-03-01 18:30:17 +0545

    HOF: forEach(), map(), filter(), reduce()

---
+++ day5.js
@@ -0,0 +1,94 @@
+// const fruits =["Apple","Dedi","Orange"];
+// //forEach
+// fruits.forEach(function(fruit){
+//     console.log(fruit)
+// })
+
+// const numbers = [1,2,3,4,5,6];
+// const squaredNumbers =  []
+// numbers.forEach(function(item){
+//     // console.log(item*item)
+//     squaredNumbers.push(item**2)
+// })
+// console.log(squaredNumbers);
+
+//map
+// const squaredNumbers =numbers.map(function(number){
+//     return number*2;
+// })
+// console.log(squaredNumbers)
+
+const users = [{
+    id:1,
+    firstName:"Dambar",
+    lastName: "Gharti",
+    address:"Nepalgunj"
+},
+{
+    id:2,
+    firstName:"Manish",
+    lastName: "Basnet",
+    address:"Itahari"
+}]
+
+const result =users.map((user)=>{
+    // return {
+    //     id:user.id,
+    //     Details: user.firstName +" lives in "+ user.lastName +" lives in " + user.address
+    // }
+    return {
+        ...user, //spread operator
+        Details: user.firstName +" "+ user.lastName +" lives in " + user.address
+    }
+})
+// console.log(result)
+
+//asignment to show output
+const numbers2 = [1,2,3,4];
+const assign = numbers2.map((item)=>{
+    return {
+        number:item,
+        name:"A"
+    }
+})
+// console.log(assign)
+
+//filter()
+const numbers = [1,2,3,4];
+const filtered = numbers.filter((number)=>{
+    return number%2 ==0 // &&
+})
+// console.log(filtered)
+
+//reduce 
+const result1 =numbers.reduce((accumulator, currentValue)=>{
+    return accumulator + currentValue
+},0)
+// console.log(result1);
+
+//daraz eg
+const cartItems =[
+    {
+        name:"A",
+        price:2,
+        quantity:4
+    },
+    {
+        name:"B",
+        price:20,
+        quantity:5
+    },
+    {
+        name:"c",
+        price:100,
+        quantity:5
+    }
+]
+//total quantity
+//total price
+const total= cartItems.reduce((acc,curr)=>{
+    acc.totalPrice +=(curr.quantity * curr.price)
+    acc.totalQuantity +=(curr.quantity)
+    return acc
+},{totalPrice:0,totalQuantity:0})
+// console.log(total)
