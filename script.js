790a50cadd1755d38aae677c70db72411f107fba
Dambar Sing <programmer.gshi18@gmail.com>
2024-03-06 23:52:00 +0545

    creating new element using js and enjecting in DOM

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h1= document.querySelector('.first')
-h1.textContent= "Hi"
-h1.innerHTML = 'hello'
+const btn = document.getElementsByTagName('button')[0]
+btn.addEventListener('dblclick',()=>{
+    h1.textContent = 'Text Changed'
+    const newParagraph = document.createElement('button');
+    newParagraph.textContent = "Text changed again"
+    document.body.appendChild(newParagraph)
+})
